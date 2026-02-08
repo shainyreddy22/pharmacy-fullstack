7155a1f7deae5d00a029b85a25e1ccb36cfdfff2
shain <karnatishainyreddy@gmail.com>
2026-02-08 22:19:48 +0530

    Create RootController.java

---
+++ backend/src/main/java/com/pharmacy/pharmacy_backend/controller/RootController.java
@@ -0,0 +1,13 @@
+package com.pharmacy.pharmacy_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Pharmacy Backend is running 🚀";
+    }
+}
